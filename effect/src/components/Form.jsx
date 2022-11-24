4c6c2a542157eeefc3162af2d7c422b523f50b4c
harishpampana7 <harishpampana7675@gmail.com>
2022-11-24 17:36:05 +0530

    day-5

---
+++ effect/src/components/Form.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+
+export const Form = () => {
+    const [counter, setCounter] = useState(0);
+    const [age, setAge] = useState(10);
+    console.log("Before Useeffect");
+
+    useEffect(() => {
+        console.log("First Effect");
+    });
+
+    useEffect(() => {
+        console.log("Second Effect");
+    }, [counter]);
+
+    useEffect(() => {
+        console.log("Third Effect");
+    }, [age]);
+
+    console.log(" After Useeffect");
+
+    return (
+        <div>
+            <h3>Counter: { counter }</h3>
+            <button onClick={() => {
+                setCounter(counter+1);
+            }}>Add 1</button>
+        </div>
+    )
+}
